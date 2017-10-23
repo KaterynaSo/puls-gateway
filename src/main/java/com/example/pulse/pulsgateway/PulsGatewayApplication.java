57b9abe3d950e41b8e06b5c7985fcbf7f5e855a6
Kateryna Sosonna X <kateryna.x.sosonna@ericsson.com>
2017-10-23 17:01:43 -0400

    Initial commit

---
+++ src/main/java/com/example/pulse/pulsgateway/PulsGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.example.pulse.pulsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsGatewayApplication.class, args);
+	}
+}
